5df599d9f927f04755436c3c9da0a274cc143f8f
marinakato-fox <marina.kato@fox-hound.jp>
2025-02-27 12:20:11 +0900

    ユーザー詳細コンポーネントの作成

--- components/UserDetails.tsx
+++ components/UserDetails.tsx
@@ -1,39 +1,20 @@
 
-// import { User } from "@/types/User";
+import { User } from "@/types/User";
+import { Typography } from "@mui/material";
 
-// interface UserDetailsProps {
-//     user: User;
-// }
+interface UserDetailsProps {
+    user: User;
+}
 
-// const UserDetails :React.FC<UserDetailsProps>= () => {
-//     const [users, setUsers] = useState<User[]>([]);
-//       const [loading, setLoading] = useState<boolean>(true);
-//       const [error, setError] = useState<string | null>(null);
-    
-//       useEffect(() => {
-//         const getUsers = async () => {
-//           try {
-//             const data = await fetchUsers();
-//             console.log(data);
-//             setUsers(data);
-//           } catch (err) {
-//             setError('ユーザーの取得に失敗しました。' + err);
-//           } finally {
-//             setLoading(false);
-//           }
-//         };
-    
-//         getUsers();
-//       }, []);
-    
-//       if (loading) {
-//         return <CircularProgress />;
-//       }
-    
-//       if (error) {
-//         return <Alert severity="error">{error}</Alert>;
-//       }
-//     return()
-// };
+const UserDetails :React.FC<UserDetailsProps>= ({user}) => {
+    return(
+        <>
+            <Typography>ID:{user.id}</Typography>
+            <Typography>名前:{user.name}</Typography>
+            <Typography>メールアドレス:{user.email}</Typography>
+            <Typography>役職:{user.role}</Typography>
+        </>
+    )
+};
 
-// export default UserDetails;
+export default UserDetails;

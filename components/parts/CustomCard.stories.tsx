e6bbadc522cd7caf4913ed9313ef767eeaea91f6
marinakato-fox <marina.kato@fox-hound.jp>
2025-02-28 15:15:17 +0900

    CustomCardコンポーネントのストーリー作成

---
+++ components/parts/CustomCard.stories.tsx
@@ -0,0 +1,35 @@
+// components/parts/CustomCard.stories.tsx
+
+import type { Meta, StoryObj } from "@storybook/react";
+import CustomCard from "./CustomCard";
+import CustomButton from "./CustomButton";
+
+// TODO: メタデータ
+const meta: Meta<typeof CustomCard> = {
+    title: "Components/Parts/CustomCard",
+    component: CustomCard,
+    tags: ["autodocs"],//このストーリーだけdocs化にしてくれる
+};
+export default meta;
+
+// TODO: ストーリーの定義
+type Story = StoryObj<typeof CustomCard>;
+export const Default: Story = {
+  args: {
+    title: "カードタイトル",
+    description: "これはカスタムカードの説明です。",
+    actions: (
+      <>
+        <CustomButton variantType="secondary">アクション1</CustomButton>
+        <CustomButton variantType="danger">アクション2</CustomButton>
+      </>
+    ),
+  },
+};
+
+export const WithoutActions: Story = {
+  args: {
+    title: "アクションなしのカード",
+    description: "アクションが含まれていないカードの説明。",
+  },
+};
